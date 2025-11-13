d286e022f894b81cab6c0689edb61d3b06b6c14a
madoha <madoha00@gmail.com>
2025-11-13 23:41:26 +0500

    feat(account): add v1 create user endpoint

--- src/modules/auth/account/inputs/create-user.input.ts
+++ src/modules/auth/account/inputs/create-user.input.ts
@@ -1,6 +1,20 @@
-import { InputType } from "@nestjs/graphql";
+import { Field, InputType } from "@nestjs/graphql";
+import { IsString, IsNotEmpty, MinLength, Matches } from "class-validator";
 
 @InputType()
 export class CreateUserInput {
+  @Field()
+  @IsString()
+  @IsNotEmpty()
+  @Matches(/^[a-zA-z0-9]+(?:-[a-zA-Z0-9]+)*$/)
   public username: string;
+
+  @Field()
+  public email: string;
+
+  @Field()
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(8)
+  public password: string;
 }
