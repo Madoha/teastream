16dc314f5ebab8e770114c188c199b732c20b4c0
madoha <madoha00@gmail.com>
2025-11-14 20:04:39 +0500

    dev branch

--- src/core/core.module.ts
+++ src/core/core.module.ts
@@ -8,6 +8,7 @@ import { getGraphQLConfig } from '../config/graphql.config'
 import { ApolloDriver } from '@nestjs/apollo'
 import { RedisModule } from './redis/redis.module';
 import { AccountModule } from '../modules/auth/account/account.module'
+import { SessionModule } from '../modules/auth/session/session.module'
 
 @Module({
 	imports: [
@@ -24,6 +25,7 @@ import { AccountModule } from '../modules/auth/account/account.module'
 		PrismaModule,
 		RedisModule,
     AccountModule,
+    SessionModule
 	],
 })
 export class CoreModule {}
