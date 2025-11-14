8e97e34cd04a8d0fe6d59b8401ccf9417381b9fe
madoha <madoha00@gmail.com>
2025-11-14 22:04:10 +0500

    feat(auth): add Redis session store

--- src/modules/auth/session/session.resolver.ts
+++ src/modules/auth/session/session.resolver.ts
@@ -1,7 +1,23 @@
-import { Resolver } from '@nestjs/graphql';
-import { SessionService } from './session.service';
+import { Args, Context, Mutation, Resolver } from '@nestjs/graphql'
+
+import type { GqlContext } from '@/src/shared/types/gql-context'
+
+import { UserModel } from '../account/models/user.model'
+
+import { LoginInput } from './inputs/login.input'
+import { SessionService } from './session.service'
 
 @Resolver('Session')
 export class SessionResolver {
-  constructor(private readonly sessionService: SessionService) {}
+	constructor(private readonly sessionService: SessionService) {}
+
+	@Mutation(() => UserModel, { name: 'loginUser' })
+	async login(@Context() { req }: GqlContext, @Args('data') input: LoginInput) {
+		return this.sessionService.login(req, input)
+	}
+
+	@Mutation(() => Boolean, { name: 'logoutUser' })
+	async logout(@Context() { req }: GqlContext) {
+		return this.sessionService.logout(req)
+	}
 }
